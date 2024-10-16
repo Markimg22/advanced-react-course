3054c255aa2d108de3a41027b84ecfe8d424f12e
Markimg22 <marquim.net.221200@hotmail.com>
2024-10-16 19:36:27 -0400

    style: apply eslint and prettier style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,13 +66,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <Image
-                        aria-hidden
-                        src="https://nextjs.org/icons/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
+                    <Image aria-hidden src="https://nextjs.org/icons/window.svg" alt="Window icon" width={16} height={16} />
                     Examples
                 </a>
                 <a
@@ -81,13 +75,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <Image
-                        aria-hidden
-                        src="https://nextjs.org/icons/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
+                    <Image aria-hidden src="https://nextjs.org/icons/globe.svg" alt="Globe icon" width={16} height={16} />
                     Go to nextjs.org →
                 </a>
             </footer>
