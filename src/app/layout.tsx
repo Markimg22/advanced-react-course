150c7f95889e2511200b4d9e89f44be64801c320
Markimg22 <marquim.net.221200@hotmail.com>
2024-11-06 14:10:48 -0400

    feat: add font Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 
 import StyledComponentsRegistry from '@/lib/registry';
 import { Providers } from '@/app/providers';
-
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900'
-});
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900'
-});
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -23,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body>
                 <StyledComponentsRegistry>
                     <Providers>{children}</Providers>
                 </StyledComponentsRegistry>
