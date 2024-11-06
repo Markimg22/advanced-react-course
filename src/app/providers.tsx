a342d35096377f95a97a99826480f5a9db388e77
Markimg22 <marquim.net.221200@hotmail.com>
2024-11-06 16:06:50 -0400

    feat: create and implement theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,16 @@
+'use client';
+
 import { PropsWithChildren } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '@/styles/global';
+import theme from '@/styles/theme';
 
 export function Providers({ children }: PropsWithChildren): JSX.Element {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <GlobalStyles />
             {children}
-        </>
+        </ThemeProvider>
     );
 }
