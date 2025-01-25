ca25792bdc8ebf39aad08e58b4fccf608d85760d
Markimg22 <marquim.net.221200@hotmail.com>
2025-01-25 09:17:10 -0400

    feat: create Footer component

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,10 @@
+import { StoryObj, Meta } from '@storybook/react';
+
+import { Footer } from '@/components/Footer';
+
+export default {
+    title: 'Footer',
+    component: Footer
+} as Meta;
+
+export const Default: StoryObj<typeof Footer> = {};
