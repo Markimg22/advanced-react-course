4a58ada805ac0fbc704654424802bd5611a248e3
Markimg22 <marquim.net.221200@hotmail.com>
2025-02-02 16:30:51 -0400

    chore: mock next image in jest

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,14 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+import React from 'react';
+
+jest.mock('next/image', () => ({
+    __esModule: true,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    default: (props: any) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { objectFit, ...rest } = props;
+        return React.createElement('img', rest);
+    }
+}));
